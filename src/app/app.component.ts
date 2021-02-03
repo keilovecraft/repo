454c28df6852c46fcbfb0645af4a0aac155b3af9
Jon Roji Taboada <joroji@MacBook-Pro-de-Jon.local>
2021-02-03 12:28:20 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-oy';
+  data = [{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    },
+    {
+      name: 'M',
+      inStock: true,
+    },
+    {
+      name: 'S',
+      inStock: false,
+    }]
+  }, {
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: false,
+    },{
+      name: 'L',
+      inStock: false,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    },
+    ,
+    {
+      name: 'M',
+      inStock: true,
+    },
+    {
+      name: 'S',
+      inStock: false,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  },{
+    name: 'Conjunto pijama tacto suave liso',
+    img: 'https://static.oysho.net/6/photos2/2021/V/3/1/b/0209/965/893/BS/BS/0209965893_6_1_18.jpg?t=1605862719481&imwidth=1920',
+    sizes: [{
+      name: 'L',
+      inStock: true,
+    }]
+  }];
 }
